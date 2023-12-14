6afaa84d6576b74436c99cc39e98418033ef0d33
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2023-12-14 20:16:09 +0530

    first commit

---
+++ school/src/main/java/edu/LearnLink/school/model/Student.java
@@ -0,0 +1,30 @@
+package edu.LearnLink.school.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Student implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String Batch;
+    private String contact;
+    private String imageUrl;
+
+    @Column(nullable = false,updatable = false)
+    private String studentId;
+
+
+
+}
